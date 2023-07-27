ad30ab6ba8ff55db2d7d732daf4333b442562acd
jennyuuu010 <115968428+jennyuuu010@users.noreply.github.com>
2023-07-27 22:00:38 +0900

    .

---
+++ Source/AIProject/AIProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AIProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIPROJECT_API AAIProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
